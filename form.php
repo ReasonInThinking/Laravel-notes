66b40abd4db2bb85f27bc5b9f3e6f15e7f3585cf
Ivanuska <107506673+ReasonInThinking@users.noreply.github.com>
2026-04-30 00:09:14 +0500

    Update and rename form to form.php

--- form.php
+++ form.php
@@ -1,4 +1,5 @@
 <form action="/handler" method="post">
+    # Cross-Site Request Forgery
     @csrf
     <input type="text" name="mood" placeholder="hello">
     <button type="submit">Click</button>
