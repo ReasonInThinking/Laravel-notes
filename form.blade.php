5073f89bdaa447b559f4300b4d2b91a7c5e8efce
Ivanuska <107506673+ReasonInThinking@users.noreply.github.com>
2026-04-30 00:12:54 +0500

    Update and rename form.php to form.blade.php

--- form.blade.php
+++ form.blade.php
@@ -1,5 +1,5 @@
 <form action="/handler" method="post">
-    // Cross-Site Request Forgery
+    {{--  Cross-Site Request Forgery --}}
     @csrf
     <input type="text" name="mood" placeholder="hello">
     <button type="submit">Click</button>
