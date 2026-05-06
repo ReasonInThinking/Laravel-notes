7dddccd6f3360cb75b166b175c92a1f11f550bf2
Ivanuska <107506673+ReasonInThinking@users.noreply.github.com>
2026-05-06 22:06:59 +0500

    Update table.sh

--- table.sh
+++ table.sh
@@ -1,2 +1,5 @@
 # Create Two Files | app/Models/Article.php | database/migrations/xxxx_create_articles_table.php
 php artisan make:model Article -m
+
+# database/migrations/xxxx_create_articles_table.php - What columns will we use in the database?
+# app/Models/Article.php - Working with data in the Database
