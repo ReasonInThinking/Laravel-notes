2e3543d895f6bf6aa0b17e4786461dee8a9bd997
Ivanuska <107506673+ReasonInThinking@users.noreply.github.com>
2026-05-06 23:13:28 +0500

    Update and rename table.sh to file for migration and  handler table columns.sh

--- file for migration and  handler table columns.sh
+++ file for migration and  handler table columns.sh
@@ -1,5 +1,3 @@
 # Create Two Files | app/Models/Article.php | database/migrations/xxxx_create_articles_table.php
 php artisan make:model Article -m
 
-# database/migrations/xxxx_create_articles_table.php 
-# app/Models/Article.php 
