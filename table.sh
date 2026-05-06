76e4d6850e929d0fedd50d9ecf3aa452e1f88ce7
Ivanuska <107506673+ReasonInThinking@users.noreply.github.com>
2026-05-06 23:12:19 +0500

    Update table.sh

--- table.sh
+++ table.sh
@@ -1,5 +1,5 @@
 # Create Two Files | app/Models/Article.php | database/migrations/xxxx_create_articles_table.php
 php artisan make:model Article -m
 
-# database/migrations/xxxx_create_articles_table.php - A file for what specific data we will send to the database.
-# app/Models/Article.php - File for working with a table (example: id, login, password)
+# database/migrations/xxxx_create_articles_table.php 
+# app/Models/Article.php 
