0d6d8e186ec1ef5b05159e162c89351ad7d2b09b
Ivanuska <107506673+ReasonInThinking@users.noreply.github.com>
2026-05-08 12:49:41 +0500

    Update form.blade.php

--- form.blade.php
+++ form.blade.php
@@ -1,3 +1,14 @@
+@if ($errors->any())
+    <div style="color: red;">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+
 <form action="/handler" method="post">
     {{--  Cross-Site Request Forgery --}}
     @csrf
