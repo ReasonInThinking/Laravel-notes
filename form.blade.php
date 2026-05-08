8337a5be12314ce88c0932ee4a790296c86c14c9
Ivanuska <107506673+ReasonInThinking@users.noreply.github.com>
2026-05-08 12:46:55 +0500

    Update form.blade.php

--- form.blade.php
+++ form.blade.php
@@ -1,6 +1,6 @@
 <form action="/handler" method="post">
     {{--  Cross-Site Request Forgery --}}
     @csrf
-    <input type="text" name="mood" placeholder="hello">
+    <input type="text" name="mood" value="{{ old('mood') }} placeholder="hello">
     <button type="submit">Click</button>
 </form>
